441a755ee7e66f23afb4f265e3c86a8a4bdd7b87
Jords-amorim <jordana_amorim@hotmail.com>
2020-09-30 19:31:22 -0300

    HttpClient - Correção
    Correção chamada Http Client

--- src/app/pay.service.ts
+++ src/app/pay.service.ts
@@ -1,25 +1,18 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-interface User {
-  id: number;
-  name: string;
-  img: string;
-  username: string;
-}
 
 @Injectable({
   providedIn: 'root'
 })
-export class PayService {
 
-  constructor(
-    private httpService: HttpClient
-    
-  ) { }
-  search () {
-    return this.httpService.get ('https://run.mocky.io/v3/533cd5d7-63d3-4488-bf8d-4bb8c751c989')
+export class PayService {    
+// Criar variável privada
+private apiurl = 'https://www.mocky.io/v2/5d531c4f2e0000620081ddce'
+
+  constructor(private http: HttpClient) { }
+
+  getData() {
+    return this.http.get(this.apiurl); 
   }
-}
-
-export {User}
+}  
--- src/app/usuarios/usuarios.component.ts
+++ src/app/usuarios/usuarios.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { from, Observable } from 'rxjs';
-import { PayService, User } from '../pay.service';
+import { PayService } from '../pay.service';
  
 
 @Component({
@@ -11,16 +10,28 @@ import { PayService, User } from '../pay.service';
 export class UsuariosComponent implements OnInit {
   
   constructor( private service: PayService) {
-    private usuarios: User [];
-  lista (){
+  //   private usuarios: User [];
+  // lista (){
    
-    this.service.search().subscribe(usuarios => {
-      console.log(usuarios[0].id)
-    })
+  //   this.service.search().subscribe(usuarios => {
+  //     console.log(usuarios[0].id)
+  //   })
+
   }
 
   ngOnInit(): void {
     //console.log(this.service.User)
+    this.service.getData().subscribe((data) => {
+      console.log("Data", data);
+
+    })
   }
 
 }
+
+// interface User {
+//   id: number;
+//   name: string;
+//   img: string;
+//   username: string;
+// }
